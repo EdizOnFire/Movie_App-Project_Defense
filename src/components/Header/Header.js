101293242ddb7b9fe2d52b2d5c4e7bf6b6952675
Ediz Redzhepov <edizonfire@gmail.com>
2022-08-16 05:48:31 +0300

    Logout functionality added
     Which replaced Logout component

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,24 +1,47 @@
-import { Link } from 'react-router-dom';
-import { useAuthContext } from '../../contexts/AuthContext';
+import { useContext } from "react";
+import { useNavigate, Link } from "react-router-dom";
+
+import { AuthContext } from "../../contexts/AuthContext";
+import * as authService from "../../services/authService";
 
 const Header = () => {
-    const { user } = useAuthContext();
+    const { user, userLogout } = useContext(AuthContext);
+    const navigate = useNavigate();
+
+    const onLogout = (e) => {
+        e.preventDefault();
+
+        authService
+            .logout(user.accessToken)
+            .then(() => {
+                navigate("/");
+                userLogout();
+            })
+            .catch(() => {
+                navigate("/");
+            });
+    };
 
     return (
         <header>
             <nav>
-                <img src='./images/movie-trans.png'></img>
+                <img src="./images/movie-trans.png"></img>
                 <Link to="/">Home</Link>
                 <ul>
                     <Link to="/catalog">Catalog</Link>
-                    {user.email
-                        ? <li><Link to="/create">Create Movie</Link>
-                            <Link to="/logout">Logout</Link>
+                    {user.email ? (
+                        <li>
+                            <Link to="/create">Create Movie</Link>
+                            <Link to="" onClick={onLogout}>
+                                Logout
+                            </Link>
                         </li>
-                        : <li><Link to="/login">Login</Link>
+                    ) : (
+                        <li>
+                            <Link to="/login">Login</Link>
                             <Link to="/register">Register</Link>
                         </li>
-                    }
+                    )}
                 </ul>
             </nav>
         </header>
