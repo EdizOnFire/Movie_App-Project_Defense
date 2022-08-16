a0d60b545a3a29ae40ed9fdd6f088e2d4f5c8dc8
Ediz Redzhepov <edizonfire@gmail.com>
2022-08-16 05:46:53 +0300

    Formating

--- src/components/Catalog/CatalogItem/CatalogItem.js
+++ src/components/Catalog/CatalogItem/CatalogItem.js
@@ -1,22 +1,24 @@
-import { Link } from 'react-router-dom';
+import { Link } from "react-router-dom";
 
 const CatalogItem = ({ item }) => {
-    return (
-        <div className="card-box">
-            <img src={item.imgUrl} />
-            <div>
-                <div className="text-center">
-                    <p className="name">Name: {item.name}</p>
-                    <p className="writer">Writer: {item.writer}</p>
-                    <p className="genre">Genre: {item.genre}</p>
-                    <p className="date">Release Date: {item.releaseDate}</p>
-                </div>
-                <div className="btn-group">
-                    <Link to={`/catalog/${item._id}`} id="details">Details</Link>
-                </div>
-            </div>
+  return (
+    <div className="card-box">
+      <img src={item.imgUrl} />
+      <div>
+        <div className="text-center">
+          <p className="name">Name: {item.name}</p>
+          <p className="writer">Writer: {item.writer}</p>
+          <p className="genre">Genre: {item.genre}</p>
+          <p className="date">Release Date: {item.releaseDate}</p>
         </div>
-    );
+        <div className="btn-group">
+          <Link to={`/catalog/${item._id}`} id="details">
+            Details
+          </Link>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default CatalogItem;
