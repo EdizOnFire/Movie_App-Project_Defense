7fd65b37fc81877e2839e35e526e6eb8c9d2b35b
Ediz Redzhepov <edizonfire@gmail.com>
2022-08-19 02:06:33 +0300

    Update App.js

--- client/src/App.js
+++ client/src/App.js
@@ -19,7 +19,6 @@ function App() {
         <AuthProvider>
             <div id="box">
                 <Header />
-
                 {/* Main Content */}
                 <ItemProvider>
                     <main id="main-content">
