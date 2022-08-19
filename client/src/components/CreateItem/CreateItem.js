fe7ffacca33e997aa2a93a946f2a74d2e8a6cd61
Ediz Redzhepov <edizonfire@gmail.com>
2022-08-19 06:00:54 +0300

    Update CreateItem.js

--- client/src/components/CreateItem/CreateItem.js
+++ client/src/components/CreateItem/CreateItem.js
@@ -21,57 +21,77 @@ const CreateItem = () => {
         <h4 className="m-0 text-uppercase font-weight-bold">Add Movie</h4>
       </div>
       <form align="center" onSubmit={onSubmit}>
-        <div className="label" htmlFor="genre" >Movie Name</div>
-        <input required="required"
+        <div className="label" htmlFor="genre">
+          Movie Name
+        </div>
+        <input
+          required="required"
           id="name"
           name="name"
           className="name"
           type="text"
           placeholder="Movie name"
         />
-        <div className="label" htmlFor="genre" >Image Url</div>
-        <input required="required"
+        <div className="label" htmlFor="genre">
+          Image Url
+        </div>
+        <input
+          required="required"
           id="imgUrl"
           name="imgUrl"
           className="imgUrl"
           type="text"
           placeholder="Image Url"
         />
-        <div className="label" htmlFor="genre" >Release Date</div>
-        <input required="required"
+        <div className="label" htmlFor="genre">
+          Release Date
+        </div>
+        <input
+          required="required"
           id="releaseDate"
           name="releaseDate"
           className="releaseDate"
           type="text"
           placeholder="Release date"
         />
-        <div className="label" htmlFor="genre" >Writer</div>
-        <input required="required"
+        <div className="label" htmlFor="genre">
+          Writer
+        </div>
+        <input
+          required="required"
           id="writer"
           name="writer"
           className="writer"
           type="text"
           placeholder="Writer"
         />
-        <div className="label" htmlFor="genre" >Genre</div>
-        <input required="required"
+        <div className="label" htmlFor="genre">
+          Genre
+        </div>
+        <input
+          required="required"
           id="genre"
           name="genre"
           className="genre"
           type="text"
           placeholder="Genre"
         />
-        <div className="label" htmlFor="genre" >Description</div>
-        <textarea required="required"
+        <div className="label" htmlFor="genre">
+          Description
+        </div>
+        <textarea
+          required="required"
           name="description"
           type="text"
           className="description"
           placeholder="Description"
         />
         <div>
-          <button className="btn btn-primary font-weight-semi-bold px-4"
+          <button
+            className="btn btn-primary font-weight-semi-bold px-4"
             style={{ height: 50 }}
-            type="submit">
+            type="submit"
+          >
             Add New Movie
           </button>
         </div>
