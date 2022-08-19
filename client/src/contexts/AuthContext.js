00d7064bba582492c4554d1749764c0f4a622bd5
Ediz Redzhepov <edizonfire@gmail.com>
2022-08-19 06:01:49 +0300

    Update AuthContext.js

--- client/src/contexts/AuthContext.js
+++ client/src/contexts/AuthContext.js
@@ -1,12 +1,10 @@
-import { createContext, useContext } from "react";
+import { createContext } from "react";
 import { useLocalStorage } from "../hooks/useLocalStorage";
 
 export const AuthContext = createContext();
 
-export const AuthProvider = ({
-    children,
-}) => {
-    const [auth, setAuth] = useLocalStorage('auth', {});
+export const AuthProvider = ({ children }) => {
+    const [auth, setAuth] = useLocalStorage("auth", {});
 
     const userLogin = (authData) => {
         setAuth(authData);
@@ -17,31 +15,15 @@ export const AuthProvider = ({
     };
 
     return (
-        <AuthContext.Provider value={{
-            user: auth,
-            userLogin,
-            userLogout,
-            isAuthenticated: !!auth.accessToken
-        }}>
+        <AuthContext.Provider
+            value={{
+                user: auth,
+                userLogin,
+                userLogout,
+                isAuthenticated: !!auth.accessToken,
+            }}
+        >
             {children}
         </AuthContext.Provider>
     );
 };
-
-// Custom Hook
-export const useAuthContext = () => {
-    const context = useContext(AuthContext);
-
-    return context;
-};
-
-// With HOC
-export const withAuth = (Component) => {
-    const AuthWrapper = (props) => {
-        const context = useContext(AuthContext);
-
-        return <Component {...props} auth={context} />
-    }
-
-    return AuthWrapper;
-} 
