498641ebe254651244bd3eb05dd32aaa408237e5
Ediz Redzhepov <edizonfire@gmail.com>
2022-08-19 06:02:16 +0300

    Update HomeItem.js

--- client/src/components/Home/HomeItem/HomeItem.js
+++ client/src/components/Home/HomeItem/HomeItem.js
@@ -1,7 +1,7 @@
 import { Link } from "react-router-dom";
 
 const HomeItem = ({ item }) => {
-
+    
     return (
         <div className="col-md-6 px-0">
             <div
@@ -15,16 +15,18 @@ const HomeItem = ({ item }) => {
                 />
                 <div className="overlay">
                     <div className="mb-2">
-                        <Link to={`/catalog/${item._id}`} id="details" className="badge badge-primary text-uppercase font-weight-semi-bold p-2 mr-2">
+                        <Link
+                            to={`/catalog/${item._id}`}
+                            id="details"
+                            className="badge badge-primary text-uppercase font-weight-semi-bold p-2 mr-2"
+                        >
                             Details
                         </Link>
                         <a className="text-white">
                             <small>{item.releaseDate}</small>
                         </a>
                     </div>
-                    <a
-                        className="h6 m-0 text-white text-uppercase font-weight-semi-bold"
-                    >
+                    <a className="h6 m-0 text-white text-uppercase font-weight-semi-bold">
                         {item.name}
                     </a>
                 </div>
