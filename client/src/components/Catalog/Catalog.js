0a3a6b880cef237b80d3d971feaa7a7aea671d3c
Ediz Redzhepov <edizonfire@gmail.com>
2022-08-19 06:00:33 +0300

    Update Catalog.js

--- client/src/components/Catalog/Catalog.js
+++ client/src/components/Catalog/Catalog.js
@@ -9,19 +9,17 @@ const Catalog = () => {
     return (
         <section id="catalogPage">
             <div className="section-title">
-                <h4 className="m-0 text-uppercase font-weight-bold">
-                    All Movies
-                </h4>
+                <h4 className="m-0 text-uppercase font-weight-bold">All Movies</h4>
             </div>
             <div className="container-fluid">
                 <div className="container">
                     <div className="row">
-                        <div className="col-12">
-                        </div>
-                        {items.length > 0
-                            ? items.map(x => <CatalogItem key={x._id} item={x} />)
-                            : <p>No Movies in Catalog!</p>
-                        }
+                        <div className="col-12"></div>
+                        {items.length > 0 ? (
+                            items.map((x) => <CatalogItem key={x._id} item={x} />)
+                        ) : (
+                            <p>No Movies in Catalog!</p>
+                        )}
                     </div>
                 </div>
             </div>
